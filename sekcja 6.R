51259741b22ae00076966fa819f884601cc1ba20
Eliza Wójcik <50324560+Wholetspugsout@users.noreply.github.com>
2020-04-05 13:32:58 +0200

    Add files via upload

--- sekcja 6.R
+++ sekcja 6.R
@@ -124,6 +124,128 @@ currency2019 <- currency2019[2:(nrow(currency2019)-4),]
 
 # 66. OPERACJE NA KOLUMNACH W DATA FRAME
 
+df <- data.frame(
+  country = c('Netherlands', 'Italy', 'Russia', 'Switzerland', 'Norway'),
+  song = c('Arcade', 'Soldi', 'Scream', 'She got me', 'Spirit in the sky'),
+  points = c(492, 465, 369, 360, 338),
+  jury = c(231,212,125,148,47)
+)
+
+
+df[1] 
+df[1,1:4] 
+df[1,] # wszystko z pierwszego wiersza i ze wszytskich kolumn
+
+df[2] # wszystko z drugiej kolumny
+
+df[[2]] # wszystko z drugiej kolumny zwrocone jako wektor
+
+summary(df) # daje podstawwe statystyki o danych
+
+top50 <- read.csv('C:/Users/Eliza/Documents/R_kurs/top50spotify2019/top50.csv',
+                  header = T, 
+                  sep =',')
+top50
+head(top50)
+
+colnames(top50)
+
+gsub('[.]','',colnames(top50)) # gsub = globalnie zamien wszytskie znaki, ktore podalismy w pierwszym argumencie (u nas to kropki)
+colnames(top50) <- gsub('[.]','',colnames(top50))
+head(top50)
+
+paste('x','y') # ³aczenie napisow, konkatenacja
+paste('x','y', sep = "_______")
+paste('x','y', sep = "")
+
+top50$Title <- paste(top50$ArtistName, top50$TrackName, sep =":") # nowa kolumna w data frame
+head(top50)
+
+sub('girl', 'boy', 'girlfriend') # znajduje pierwszy argument w trzecim argumencie i zamienia go na drugi argument. Uwaga! case sensitive
+top50$TrackName
+sub('[Yy]ou', 'I', top50$TrackName) # [Yy] powoduje ze bierze pod uwage zarowno napis z malej litery y jak i z du¿ej Y
+
+gsub('[Gg]irl','boy', 'Girlfriend')
+gsub('girl','boy', 'Girlfriend', ignore.case = T) # tez bedzie bralo pod uwage duza i mala lietra
+
+top50$len_category <- ifelse(top50$Length>= median(top50$Length), "LONG", "SHORT" )
+top50$len_category
+
+top50$len_category <- ifelse(top50$len_category == "LONG", 1,0) # jesli tak - 1, jesli nie - 0 
+
+# 67 LAB 
+
+coffebar <- data.frame(
+  name = c('Caffe Americano', 'Cafe Latte','Cappuccino','Espresso','Flat white','Macchiato'),
+  ingredients = c(NA,'steamed milk','stemed milk + foamy milk + chocolate',NA,'cream milk','steamed milk'),
+  country = c('USA','France','Italy','Italy','New Zealand','Italy'),
+  temp = c('Hot','Hot','Medium hot','Hot','Hot','Hot'),
+  price = c(3, 4, 4, 3, 5, 4)
+)
+
+coffebar[3] # lista
+coffebar[[3]] # wektor
+summary(coffebar)
+
+
+whiskey <- read.csv('C:/Users/Eliza/Documents/R_kurs/scotish-whisky-score/scotch_score.csv')
+gsub('[.]','_', colnames(whiskey))
+tolower(colnames(whiskey))
+whiskey$origin <- paste(whiskey$REGION,whiskey$DISTRICT, sep= "/")
+head(whiskey)
+
+whiskyDF$origin <- gsub('high/','',whiskyDF$origin, ignore.case = TRUE)
+head(whiskyDF,n=20)
+
+
+whiskyDF$UpDown <- ifelse(whiskyDF$score>0, '+', '-')
+head(whiskyDF,n=20)
+
+# 69 FILTROWANIE DANCYH
+
+# wyrazenia logiczne
+
+1 > 2
+1< 2
+1 == 2 # czy wartosci sa sobie rowne
+1 != 2 # czy te wartosci sa od siebie rozne
+! 1 != 2 # nie prawda, ze 1 jest roznem od 2 
+!! 1 != 2 # nie prawda, ze nie prawda, ze 1 jest roznem od 2
+1 >= 2
+1 > 2 & 2 ==2 # jeden mniejsze od dwa i jednoczesnie 2 rowna sie 2
+1 > 2 & 2 !=2
+"pilot" =="pilot"
+'pilot' == "pILot"
+toupper('pilot') == toupper("pILot")
+&& # bedzie pracowal na pojedynczych wartosciach
+& # zadziala na wektorze
+
+  
+  
+top50 <- read.csv('C:/Users/Eliza/Documents/R_kurs/top50spotify2019/top50.csv',
+                    header = T, 
+                    sep =',')
+colnames(top50) <- gsub('[.]','',colnames(top50))
+head(top50)
+
+# filtrowanie
+
+top50$Genre == 'pop'
+top50$Genre[c(T)]
+top50$Genre[top50$Genre == 'pop']  
+
+filter_pop <- top50$Genre == 'pop'
+filter_pop
+top50$Genre[filter_pop]
+top50$TrackName[filter_pop]  
+
+top50[filter_pop, c("ArtistName", 'TrackName')]  
+top50[filter_pop,] # wyswieti wszytsko co spelnia warunek filter pop 
+  
+
+
+
+
 
 
 
